c462986e3b69985f1901bfe7b83897aa89692207
Njf092024 <Njf092024@gmail.com>
2024-11-29 13:56:13 +0100

    Add logic to seed the in-memory database with initial Champion data

--- LeagueOfAPI/Program.cs
+++ LeagueOfAPI/Program.cs
@@ -7,3 +7,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AppDbContext<AppDbContext>(options => options.UseInMemoryDatabase("LeagueDb"));
 
 var app = builder.Build();
+
+using(var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Champions.AddRange(
+        new Champion {id = 1, Name = "Ahri", Role = "Mage", Difficulty = 3 },
+        new Champion {id = 2, Name = "Yasuo", Role = "Fighter", Difficulty = 5 },
+        new Champion {id = 3, Name = "Thresh", Role = "Support", Difficulty = 4 }
+
+    );
+db.SaveChanges();
+}
