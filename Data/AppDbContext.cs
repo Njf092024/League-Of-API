f2a36e42eb033b5d8a8161c22541094009acadd3
Njf092024 <Njf092024@gmail.com>
2024-11-29 13:48:02 +0100

    Add AppDbContext with DbSet for Champion management

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,2 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using LeagueOfAPI.Models;
+
+namespace LeagueOfAPI.Data;
+
+public class AppDbContext : AppDbContext
+{
+    public DbSet<Champion> Champions { get; set; }
+
+    public AppDbContext(AppDbContextOptions<AppDbContext> options) : base(options) { }
+}
