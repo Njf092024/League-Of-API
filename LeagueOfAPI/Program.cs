e519ebd4cc4cd1e15ba4e91535809a14a0c204e1
Njf092024 <Njf092024@gmail.com>
2024-11-29 13:52:12 +0100

    Add logic to configure in-memory database and build the application

--- LeagueOfAPI/Program.cs
+++ LeagueOfAPI/Program.cs
@@ -3,3 +3,7 @@ using LeagueOfAPI.Models;
 using LeagueOfAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AppDbContext<AppDbContext>(options => options.UseInMemoryDatabase("LeagueDb"));
+
+var app = builder.Build();
