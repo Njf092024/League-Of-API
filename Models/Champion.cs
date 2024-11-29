fef1a3beaa9ce9f9c8a8aad623e7afb137bcdd8e
Njf092024 <Njf092024@gmail.com>
2024-11-29 13:43:08 +0100

    Add Champion model with properties Id, Name, Role, and Difficulty

---
+++ Models/Champion.cs
@@ -0,0 +1,9 @@
+namespace LeagueOfAPI.Models;
+
+public class Champion
+{
+public int Id {get; set; }
+public string Name {get; set; } = string.Empty;
+public string Role {get; set; } = string.Empty;
+public int Difficulty { get; set; }
+}
